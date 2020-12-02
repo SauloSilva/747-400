0799a0d0edbaaed8b55b4125965195238fa64800
MisterFahrenhiet <62818033+MisterFahrenhiet@users.noreply.github.com>
2020-12-02 15:39:40 -0500

    Add files via upload
    Central Maintenance Computer functions

---
+++ plugins/xtlua/scripts/B747.68.xt.fms/activepages/B744.fms.pages.cmc.lua
@@ -0,0 +1,34 @@
+fmsPages["CMC"]=createPage("CMC")
+fmsPages["CMC"]["template"]={
+  if pgNo==1 then return {
+            
+"     CMC-L MENU      1/2",
+"                        ",
+"<PRESENT LEG FAULTS     ",
+"                        ",
+"<CONFIDENCE TESTS       ",
+"                        ",
+"<EICAS MANIT PAGES      ",
+"                        ",
+"<GROUND TESTS           ",
+"                        ",
+"                        ", 
+"------------------------",
+"                   HELP>"
+    }
+  elseif pgNo==2 then return {
+            
+"     CMC-L MENU      2/2",
+"                        ",
+"<EXISTING FAULTS        ",
+"                        ",
+"<FAULT HISTORY          ",
+"                        ",
+"<OTHER FUNCTIONS        ",
+"                        ",
+"------------------------",
+"                        ",
+"                  NOTES>", 
+"                        ",
+"                   HELP>"
+    }
