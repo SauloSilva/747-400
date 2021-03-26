947a18e452d32d9f3fabd0552245f9f6bd51e1b0
gHashFlyer <77414195+gHashFlyer@users.noreply.github.com>
2021-03-26 09:38:03 -0400

    Cleaned up formatting

--- plugins/xtlua/scripts/B747.68.xt.fms/activepages/B744.fms.pages.vnav.lua
+++ plugins/xtlua/scripts/B747.68.xt.fms/activepages/B744.fms.pages.vnav.lua
@@ -299,7 +299,8 @@ fmsPages["VNAV"].getPage=function(self,pgNo,fmsID)--dynamic pages need to be thi
       fmsFunctionsDefs["VNAV"]["R1"]=nil
       fmsFunctionsDefs["VNAV"]["R3"]=nil
 
-      if B747DR_ap_vnav_state>0 and simDR_autopilot_vs_status==2 then
+      -- cleaned up formatting a little
+      if B747DR_ap_vnav_state==2 and simDR_autopilot_vs_status==2 then
 	      fmsModules["data"]["fpa"]=string.format("%1.1f",B747DR_ap_fpa)
 	      fmsModules["data"]["vb"]=string.format("%1.1f", B747DR_ap_vb)
 	      fmsModules["data"]["vs"]=string.format("%4d",simDR_autopilot_vs_fpm*-1) 
