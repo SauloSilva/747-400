6ff6009f44f9bc80d073e1c27b8988bfbd88c9b6
crazytimtimtim <64425497+crazytimtimtim@users.noreply.github.com>
2021-06-01 17:16:51 -0500

    Update num(Lever)Climb

--- plugins/xtlua/init/scripts/B747.45.fltCtrls/B747.45.fltCtrls.lua
+++ plugins/xtlua/init/scripts/B747.45.fltCtrls/B747.45.fltCtrls.lua
@@ -81,6 +81,9 @@ B747DR_init_fltctrls_CD         = deferred_dataref("laminar/B747/fltctrls/init_C
 B747DR_parking_brake_ratio      = deferred_dataref("laminar/B747/flt_ctrls/parking_brake_ratio", "number")
 B747DR_CAS_memo_status          = find_dataref("laminar/B747/CAS/memo_status")
 
+B747DR_engines_numClimb         = deferred_dataref("laminar/B747/flt_ctrls/numClimb", "number")
+B747DR_engines_numLeverClimb    = deferred_dataref("laminar/B747/flt_ctrls/numLeverClimb", "number")
+
 --*************************************************************************************--
 --** 				       READ-WRITE CUSTOM DATAREF HANDLERS     	        	     **--
 --*************************************************************************************--
@@ -162,5 +165,3 @@ B747CMD_ai_fltctrls_quick_start			= deferred_command("laminar/B747/ai/fltctrls_q
 B747CMD_parking_brake_on            = deferred_command("laminar/B747/flt_ctrls/parking_brake_on", "Parking brake on", B747CMD_parking_brake_on_CMDhandler)
 B747CMD_parking_brake_off            = deferred_command("laminar/B747/flt_ctrls/parking_brake_off", "Parking brake off", B747CMD_parking_brake_off_CMDhandler)
 B747CMD_parking_brake_toggle            = deferred_command("laminar/B747/flt_ctrls/parking_brake_toggle", "Parking brake toggle", B747CMD_parking_brake_toggle_CMDhandler)
-
-
